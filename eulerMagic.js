825830bcd7b62a3fa576c39e85606fd2e3e12519
Leon <leonluleonlu@gmail.com>
2019-09-13 15:17:25 +0800

    fix flaw

--- eulerMagic.js
+++ eulerMagic.js
@@ -3,9 +3,9 @@ function dyOVERdx(x, y) {
 }
 function eulerMagic(x0, y0, step, target) {
   var y1 = y0;
-  for (i = 0; i < ((target - x0) / step); i++) {
+  for (i = 0; i <= ((target - x0) / step); i++) {
+    console.log("[ step = " + i + " | x = " + (x0 + (i * step)) + " | y = " + y1 + " | dy/dx = " + dyOVERdx(x0 + (i * step), y1) + " | delta y = " + (step * dyOVERdx(x0 + (i * step), y1)) + " ]");
     y1 = y1 + step * dyOVERdx(x0 + (i * step), y1);
-    console.log("[ step = " + (i + 1) + " | x = " + (x0 + (i * step)) + " | y = " + y1 + " | dy/dx = " + dyOVERdx(x0 + (i * step), y1) + " | delta y = " + (step * dyOVERdx(x0 + (i * step), y1)) + " ]");
   }
   return y1;
 }
